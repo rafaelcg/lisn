21d72f3f11629b8636506b8f2ce8f7c2a23739c3
Rafael Cammarano Guglielmi <rafaelcg@gmail.com>
2026-03-10 02:21:57 +0000

    Package renderer assets in release builds

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -5,6 +5,7 @@ import baseConfig from "./vite.base.config";
 
 const rendererRoot = fileURLToPath(new URL("./src/renderer", import.meta.url));
 const rendererEntry = fileURLToPath(new URL("./src/renderer/index.html", import.meta.url));
+const rendererOutDir = fileURLToPath(new URL("./.vite/renderer/main_window", import.meta.url));
 
 export default mergeConfig(
   baseConfig,
@@ -12,6 +13,7 @@ export default mergeConfig(
     root: rendererRoot,
     plugins: [react()],
     build: {
+      outDir: rendererOutDir,
       rollupOptions: {
         input: rendererEntry
       }
